ab70248718bc1b404c10bee1a8af2c47f5c59c34
Mohammad Shafiqur Rahman <90058132+shafiqafat@users.noreply.github.com>
2023-09-09 15:12:09 +0600

    Add files via upload

---
+++ StringInput.java
@@ -0,0 +1,15 @@
+package stringinput;
+
+import java.util.Arrays;
+
+public class StringInput {
+    public static void main(String[] args) {
+        int[] ints = new int[5];
+        System.out.println("ints = " + Arrays.toString(ints));
+        Integer[] integers = new Integer[5];
+        System.out.println("integers = " + Arrays.toString(integers));
+        float[] floats = new float[5];
+        System.out.println("floats = " + Arrays.toString(floats));
+    }
+    
+}
