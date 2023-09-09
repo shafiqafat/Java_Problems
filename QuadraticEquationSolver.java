415453dda630bd93ae602f58b35609f43a5afc8d
Mohammad Shafiqur Rahman <90058132+shafiqafat@users.noreply.github.com>
2023-09-09 15:14:38 +0600

    Add files via upload

---
+++ QuadraticEquationSolver.java
@@ -0,0 +1,37 @@
+package quadraticequationsolver;
+//For 2x^2+5x-3=0
+
+import java.util.Scanner;
+
+
+public class QuadraticEquationSolver {
+
+   
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        int a = input.nextInt();
+        System.out.print("Enter the value of b: ");
+        int b = input.nextInt();
+        System.out.print("Enter the value of c: ");
+        int c = input.nextInt();
+        
+        double determinant = b*b - 4.0*a*c;
+        
+        if(determinant > 0.0){
+            double root1 = (-b + Math.sqrt(determinant))/(2.0*a);
+            double root2 = (-b - Math.sqrt(determinant))/(2.0*a);
+            
+            System.out.println("The roots are " +root1+" and "+root2);
+        }
+        else if(determinant == 0.0){
+            double root = -b/(2.0*a);
+            System.out.println("The root is "+root);
+        }
+        else{
+            System.out.println("The equation has no roots.");
+        }
+    }
+    
+}
