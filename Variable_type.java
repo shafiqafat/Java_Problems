2f64f9563c330f680996a7656a637939c9b3466d
Mohammad Shafiqur Rahman <90058132+shafiqafat@users.noreply.github.com>
2023-09-09 15:06:25 +0600

    Create Variable_type.java

---
+++ Variable_type.java
@@ -0,0 +1,24 @@
+package com.shafiq;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int a = 2;
+        float f = 3.0f;
+        double d = 32.225d;
+        long l = 32321332321554L;
+        short sh = 32622;
+        boolean b = true;
+        String s = "Oka";
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(l);
+        System.out.println(sh);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
