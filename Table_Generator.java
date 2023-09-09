429b26b268afea35e6a5555d51fafa0fe1e78c07
Mohammad Shafiqur Rahman <90058132+shafiqafat@users.noreply.github.com>
2023-09-09 15:05:14 +0600

    Create Table_Generator.java

---
+++ Table_Generator.java
@@ -0,0 +1,22 @@
+package com.shafiq;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        String table = """                         
+                +-------------------+-----------------------+-------------+----------------+
+                |BOOKS              |WRITER                 |QUANTITY     |PRICE           |
+                +-------------------+-----------------------+-------------+----------------+
+                |HIMU               |HUMAYUN AHMED          |01           |155/-           |
+                |SOBNOM             |AHMED SOFA             |01           |175/-           |
+                |AMI TOPU           |MOHAMMAD JAFOR IQBAL   |01           |150/-           |
+                |CHOKHER BALI       |ROBINDRONATH THAGOR    |01           |220/-           |
+                +-------------------+-----------------------+-------------+----------------+
+                |TOTAL                                      |05           |700/-           |
+                +-------------------+-----------------------+-------------+----------------+
+                 """;
+        System.out.println(table);
+        //textblock
+    }
+}
