bb92e8f332f3b1d1e94bf691f2acb0cc1170c73f
Mohammad Shafiqur Rahman <90058132+shafiqafat@users.noreply.github.com>
2023-09-09 15:19:58 +0600

    Delete shafiq directory

--- shafiq/PiCalculator.java
+++
@@ -1,9 +0,0 @@
-package com.shafiq;
-
-public class PiCalculator {
-    public static void main(String[] args) {
-        PiCalculator piCalculator = new PiCalculator();
-        var pi = piCalculator.calculate(100_000);
-        System.out.println("pi = " + pi);
-    }
-}
