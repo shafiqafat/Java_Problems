e1f8127a53c3fa0e5fe7354151dd807950f26e04
Mohammad Shafiqur Rahman <90058132+shafiqafat@users.noreply.github.com>
2023-09-09 15:25:03 +0600

    Add files via upload

---
+++ JavatoASCII.java
@@ -0,0 +1,11 @@
+package javatoascii;
+public class JavatoASCII 
+{
+    public static void main(String[] args)
+    {
+        char a = 'a';
+        int asciiValue = a;
+        System.out.println("ASCIIValue of a: "+asciiValue);
+    }
+    
+}
